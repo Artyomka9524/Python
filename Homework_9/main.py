590b3f5a8b530938c641b8cef81cc39bc9819842
Artyom <csgo1320@gmail.com>
2022-11-06 22:51:20 +0300

    XO

--- Homework_9/main.py
+++ Homework_9/main.py
@@ -1,145 +1,88 @@
-import os
-import random
-import sys
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, filters
+from telegram import ReplyKeyboardRemove
+from settings import token
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+)
 
-import strings as st
+fld = list(range(1, 10))
+x = chr(10060)
+o = chr(11093)
+count = 9
+player = x
+CHOICE = 0
 
 
-# поиск токена для телеграмма
-def getToken():
-    token = ''
-    if os.path.isfile(st.BOT_TOKEN_FILENAME):
-        f = open(st.BOT_TOKEN_FILENAME, "r")
-        token = f.read()
-        f.close()
+def show_field(field):
+    txt = ''
+    for i in range(len(field)):
+        if not i % 3:
+            txt += f'\n{"-" * 25}\n'
+        txt += f'{field[i]:^8}'
+    txt += f"\n{'-' * 25}"
+    return txt
+
+
+def check_win(field):
+    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    n = [field[x[0]] for x in win_coord if field[x[0]] == field[x[1]] == field[x[2]]]
+    return n[0] if n else n
+
+def start(update, _):
+    global fld, player, count
+    fld = list(range(1, 10))
+    count = 9
+    player = x
+    update.message.reply_text("Привет, давай сыграем в крестики-нолики")
+    update.message.reply_text(show_field(fld))
+    update.message.reply_text(f'Ходит первый{chr(10060)}')
+    return CHOICE
+
+
+def choice(update, _):
+    global player, count
+    move = update.message.text
+    move = int(move)
+    if move not in fld:
+        update.message.reply_text(f"Некорректный ввод{chr(9940)}\nПовторите попытку")
     else:
-        print("Пожалуйста, создайте в папке проекта файл 'token.txt' и поместите туда токен для работы телеграм бота и запустите скрипт заново")
-        sys.exit() 
-    return token
+        fld.insert(fld.index(move), player)
+        fld.remove(move)
+        update.message.reply_text(show_field(fld))
+        if check_win(fld):
+            update.message.reply_text(f"{player} - Победитель{chr(127942)}")
+            return ConversationHandler.END
+        player = o if player == x else x
+        count -= 1
 
-# проверка на выигрыш
-def isWin(arr, who):
-    if (((arr[0] == who) and (arr[4] == who) and (arr[8] == who)) or
-            ((arr[2] == who) and (arr[4] == who) and (arr[6] == who)) or
-            ((arr[0] == who) and (arr[1] == who) and (arr[2] == who)) or
-            ((arr[3] == who) and (arr[4] == who) and (arr[5] == who)) or
-            ((arr[6] == who) and (arr[7] == who) and (arr[8] == who)) or
-            ((arr[0] == who) and (arr[3] == who) and (arr[6] == who)) or
-            ((arr[1] == who) and (arr[4] == who) and (arr[7] == who)) or
-            ((arr[2] == who) and (arr[5] == who) and (arr[8] == who))):
-        return True
-    return False
+    if count == 0:
+        update.message.reply_text(f"Ничья {chr(129309)}")
+        return ConversationHandler.END
 
 
-# возвращает количество неопределенных ячеек (т.е. количество ячеек, в которые можно сходить)
-# cellArray - массив данных из callBackData, полученных после нажатия на callBack-кнопку
-def countUndefinedCells(cellArray):
-    counter = 0
-    for i in cellArray:
-        if i == st.SYMBOL_UNDEF:
-            counter += 1
-    return counter
-
-def game(callBackData):
-    # -------------------------------------------------- global message  # использование глобальной переменной message
-    message = st.ANSW_YOUR_TURN  # сообщение, которое вернется
-    alert = None
-
-    buttonNumber = int(callBackData[0])  # считывание нажатой кнопки, преобразуя ее из строки в число
-    if not buttonNumber == 9:  # цифра 9 передается в первый раз в качестве заглушки. Т.е. если передана цифра 9, то клавиатура для сообщения создается впервые
-        charList = list(callBackData)  # строчка callBackData разбивается на посимвольный список "123" -> ['1', '2', '3']
-        charList.pop(0)  # удаление из списка первого элемента: который отвечает за выбор кнопки
-        if charList[buttonNumber] == st.SYMBOL_UNDEF:  # проверка: если в нажатой кнопке не выбран крестик/нолик, то можно туда сходить крестику
-            charList[buttonNumber] = st.SYMBOL_X  # эмуляция хода крестика
-            if isWin(charList, st.SYMBOL_X):  # проверка: выиграл ли крестик после своего хода
-                message = st.ANSW_YOU_WIN
-            else:  # если крестик не выиграл, то может сходит бот, т.е. нолик
-                if countUndefinedCells(charList) != 0:  # проверка: есть ли свободные ячейки для хода
-                    # если есть, то ходит бот (нолик)
-                    isCycleContinue = True
-                    # запуск бесконечного цикла т.к. необходимо, чтобы бот походил в свободную клетку, а клетка выбирается случайным образом
-                    while (isCycleContinue):
-                        rand = random.randint(0, 8)  # генерация случайного числа - клетки, в которую сходит бот
-                        if charList[rand] == st.SYMBOL_UNDEF:  # если клетка неопределенна, то ходит бот
-                            charList[rand] = st.SYMBOL_O
-                            isCycleContinue = False  # смена значения переменной для остановки цикла
-                            if isWin(charList, st.SYMBOL_O):  # проверка: выиграл ли бот после своего кода
-                                message = st.ANSW_BOT_WIN
-
-        # если клетка, в которую хотел походить пользователь уже занята:
-        else:
-            alert = st.ALERT_CANNOT_MOVE_TO_THIS_CELL
-
-        # проверка: остались ли свободные ячейки для хода и что изначальное сообщение не поменялось (означает, что победителя нет, и что это был не ошибочный ход)
-        if countUndefinedCells(charList) == 0 and message == st.ANSW_YOUR_TURN:
-            message = st.ANSW_DRAW
-
-        # формирование новой строчки callBackData на основе сделанного хода
-        callBackData = ''
-        for c in charList:
-            callBackData += c
-
-    # проверка, что игра закончилась (message равно одному из трех вариантов: победил Х, 0 или ничья):
-    if message == st.ANSW_YOU_WIN or message == st.ANSW_BOT_WIN or message == st.ANSW_DRAW:
-        message += '\n'
-        for i in range(0, 3):
-            message += '\n | '
-            for j in range(0, 3):
-                message += callBackData[j + i * 3] + ' | '
-        callBackData = None  # обнуление callBackData
-
-    return message, callBackData, alert
-
-def getKeyboard(callBackData):
-    keyboard = [[], [], []]  # заготовка объекта клавиатуры, которая вернется
-
-    if callBackData != None:  # если
-        # формирование объекта клавиатуры
-        for i in range(0, 3):
-            for j in range(0, 3):
-                keyboard[i].append(InlineKeyboardButton(callBackData[j + i * 3], callback_data=str(j + i * 3) + callBackData))
-
-    return keyboard
-
-
-def newGame(update, _):
-    # сформировать callBack данные для первой игры, то есть строку, состояющую из 9 неопределенных символов
-    data = ''
-    for i in range(0, 9):
-        data += st.SYMBOL_UNDEF
-
-   
-    update.message.reply_text(st.ANSW_YOUR_TURN, reply_markup=InlineKeyboardMarkup(getKeyboard(data)))
-
-
-def button(update, _):
-    query = update.callback_query
-    callbackData = query.data  
-
-    message, callbackData, alert = game(callbackData)  # игра
-    if alert is None:  
-        query.answer()  
-        query.edit_message_text(text=message, reply_markup=InlineKeyboardMarkup(getKeyboard(callbackData)))
-    else:  
-        query.answer(text=alert, show_alert=True)
-
-
-def help_command(update, _):
-    update.message.reply_text(st.ANSW_HELP)
+def cancel(update, _):
+    update.message.reply_text('До свидания', reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
 
 
 if __name__ == '__main__':
-    updater = Updater(getToken()) 
+    updater = Updater(token)
+    dispatcher = updater.dispatcher
 
-    # добавление обработчиков
-    updater.dispatcher.add_handler(CommandHandler('start', newGame))
-    updater.dispatcher.add_handler(CommandHandler('new_game', newGame))
-    updater.dispatcher.add_handler(CommandHandler('help', help_command))
-    updater.dispatcher.add_handler(MessageHandler(filters.TEXT, help_command))  
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))  
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            CHOICE: [MessageHandler(Filters.text, choice)]
+        },
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
 
-    # Запуск бота
+    dispatcher.add_handler(conv_handler)
+
+    print('server start')
+
     updater.start_polling()
     updater.idle()
